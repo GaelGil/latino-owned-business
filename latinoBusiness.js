7e27e26857d21a0e0bbc8c99948922bd0013262f
GaelGil <gaelgilbello27@gmail.com>
2018-10-18 21:41:46 -0700

    made items display, figure out how to display inline and put them all in container

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -1,11 +1,16 @@
-console.log('whats up')
 $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ3080rrDaCwO6',
-function(data){
-    $.each(data.records, function(index, record){
-    var name = record.fields['Name'];
-    var type = record.fields['Type'];
-    var html = '<p>${name}, ${type}</p>';
-    $('body').append(html);
+  function(data){
+    var html = [];
+    $.each(data.records, function(index, record) {
+      var name = record.fields['Name'];
+      var location = record.fields['Location'];
+      var type = record.fields['Type'];
+      //html.push(`<div class="name"> ${name}</div>`);
+      //html.push(`<div class="type">${type}<div>`);
+      //html.push(`<div class="location">${location}</div>`);
+      html.push(`<div class="businesses"><h2><div class="name">${name},</div> <div class="type">${type},</div> 
+      <div class="location">${location}</div></h2></div>`);
     });
+    $('body').append(html);
   }
 );
