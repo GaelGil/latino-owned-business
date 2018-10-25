5a6e2a32cb76d5c26ffd0dcd7abbd5f396883ecb
GaelGil <31393159+GaelGil@users.noreply.github.com>
2018-10-25 11:15:48 -0700

    Changed back to normal

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -9,15 +9,15 @@ $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ
       var name = record.fields['Name'];
       var location = record.fields['Location'];
       var type = record.fields['Type'];
-      var img = record.fields['Images'];
-      html.push(`<img src="${img}">`);
+      //var img = record.fields['Images'];
+      //html.push(`<img src="${img}">`);
       //html.push(`<div class="name"> ${name}</div>`);
       //html.push(`<div class="type">${type}<div>`);
       //html.push(`<div class="location">${location}</div>`);
-      //html.push(`<ul><li><div class="businesses"><h2><div class="name">${name},</div> <div class="type">${type},</div> 
-      //<div class="location">${location}</div></h2></div></li></ul>`);
+      html.push(`<ul><li><div class="businesses"><h2><div class="name">${name},</div> <div class="type">${type},</div> 
+      <div class="location">${location}</div></h2></div></li></ul>`);
     });
-    $('body').append(html);
+    $('.list').append(html);
   }
 ); 
  
