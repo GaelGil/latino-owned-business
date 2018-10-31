71a6a326f48f596d908c0d3c3a59fac66938942f
GaelGil <gaelgilbello27@gmail.com>
2018-10-30 18:04:40 -0700

    added some imgs, made them inline

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -10,11 +10,12 @@ $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ
       var location = record.fields['Location'];
       var type = record.fields['Type'];
       var img = record.fields['Images'];
-      html.push(`<div class="businesses"><img style="width: 300px; height: 150px;"src="${img}"></div>`);
+      //html.push(`<div class="businesses"><img style="width: 300px; height: 150px;"src="${img}"></div>`);
       //html.push(`<div class="name"> ${name}</div>`);
       //html.push(`<div class="type">${type}<div>`);
       //html.push(`<div class="location">${location}</div>`);
-      html.push(`<ul><li><div class="businesses"><h2><div class="name">${name},</div> <div class="type">${type},</div> 
+      html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
+      </div><div class="name">${name},</div> <div class="type">${type},</div> 
       <div class="location">${location}</div></h2></div></li></ul>`);
     });
     $('.list').append(html);
