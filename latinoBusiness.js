50016e8f5a92587e3104388289bcdd18f5d6a44f
GaelGil <gaelgilbello27@gmail.com>
2018-10-30 18:42:47 -0700

    added a small border to captions

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -15,7 +15,7 @@ $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ
       //html.push(`<div class="type">${type}<div>`);
       //html.push(`<div class="location">${location}</div>`);
       html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
-      </div><div class="name">${name},</div> <div class="type">${type},</div> 
+      </div><div class="name">${name}</div> <div class="type">${type}</div> 
       <div class="location">${location}</div></h2></div></li></ul>`);
     });
     $('.list').append(html);
