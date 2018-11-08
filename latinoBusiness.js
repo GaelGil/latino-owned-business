ae18fd4b8b3c706ef4dda9fda3bd273ac1cd985f
GaelGil <gaelgilbello27@gmail.com>
2018-11-07 17:17:00 -0800

    commented out my way of displaying grid

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -10,13 +10,11 @@ $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ
       var location = record.fields['Location'];
       var type = record.fields['Type'];
       var img = record.fields['Images'];
-      //html.push(`<div class="businesses"><img style="width: 300px; height: 150px;"src="${img}"></div>`);
-      //html.push(`<div class="name"> ${name}</div>`);
-      //html.push(`<div class="type">${type}<div>`);
-      //html.push(`<div class="location">${location}</div>`);
-      html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
+      /*in case all fails use this */
+      /*html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
       </div><div class="name">${name}</div> <div class="type">${type}</div> 
-      <div class="location">${location}</div></h2></div></li></ul>`);
+      <div class="location">${location}</div></h2></div></li></ul>`);*/
+      html.push(`<h2>${name}, ${type}, ${location}</h2>`);
     });
     $('.list').append(html);
   }
