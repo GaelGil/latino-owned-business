539f6270ea1cd7a6fa6bca77284fd81bf611fb61
GaelGil <gaelgilbello27@gmail.com>
2018-11-07 18:58:15 -0800

    did some cray cray stuff

--- latinoBusiness.js
+++ latinoBusiness.js
@@ -10,13 +10,27 @@ $.getJSON('https://api.airtable.com/v0/app7wsmtFYHxb5dNF/Table%201?api_key=keyjQ
       var location = record.fields['Location'];
       var type = record.fields['Type'];
       var img = record.fields['Images'];
-      /*in case all fails use this */
-      /*html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
-      </div><div class="name">${name}</div> <div class="type">${type}</div> 
-      <div class="location">${location}</div></h2></div></li></ul>`);*/
-      html.push(`<h2>${name}, ${type}, ${location}</h2>`);
+      var photos = record.fields['Photos'];
+      html.push(listView(name, type, location, img,));
     });
     $('.list').append(html);
   }
 ); 
- 
+
+var listView = function(name, type, location, img) {
+  return `
+    <img src="${img}"; alt="photo">
+    <h2>${name}</h2>
+    <p>${type}</p>
+    <p>${location}</p>
+    `;
+}
+
+
+
+      /*in case all fails use this */
+      /*html.push(`<ul><li><div class="businesses"><h2><div class="img"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">
+      </div><div class="name">${name}</div> <div class="type">${type}</div> 
+      <div class="location">${location}</div></h2></div></li></ul>`);*/
+      /*in case all fails pt 2
+      html.push(`<div class="container"><img style="width: 300px; height: 150px;"src="${img}" alt="photo">, ${name}, ${type}, ${location}</div>`);*/
