005e6d773673b69404e217ce415baad01b8effa4
Gael Gil <gaelgilbello27@gmail.com>
2019-01-10 17:06:17 -0800

    made navbar

--- sticky.js
+++ sticky.js
@@ -1,7 +1,7 @@
 $(window).scroll(function(){
     if($(this).scrollTop()>100) { //cheks if scrollin
-        $('.menu').addClass("sticky"); //if scroll "sticky" will be used
+        $('.nav-bar').addClass("sticky"); //if scroll "sticky" will be used
     } else{
-        $('.menu').removeClass("sticky"); //when not scrolling "sticky" isnt being use
+        $('.nav-bar').removeClass("sticky"); //when not scrolling "sticky" isnt being use
     }
 })
