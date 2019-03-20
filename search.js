5c42e70beee3f3b7424a97f8da1d339bc21dee45
Gael Gil <gaelgilbello27@gmail.com>
2019-03-20 16:34:31 -0700

    added a comment

--- search.js
+++ search.js
@@ -2,7 +2,7 @@
 //on the type
 function filter () {
 
-    var value,type, card, fullCard, i; //variables used 
+    var value,type, card, fullCard, i; //variables used
     value = document.getElementById("value").value.toUpperCase();
     card = document.getElementsByClassName("card-text");
     fullCard = document.getElementsByClassName("card");
@@ -10,23 +10,23 @@ function filter () {
 
     for(i=0; i <fullCard.length; i++){
         type = fullCard[i].getElementsByClassName("type");
-        if  (type[0].innerHTML.toUpperCase().indexOf(value)){ 
-             fullCard[i].style.display = "none"; //originaly flex 
+        if  (type[0].innerHTML.toUpperCase().indexOf(value)){
+             fullCard[i].style.display = "none"; //originaly flex
         } else {
             $('.card').addClass(".search")
             //fullCard[i].style.display = "grid";
-        } 
+        }
     }
 }
 
 
 
 
-//works for now but 
-//when i type in 
-//a value in it 
+//works for now but
+//when i type in
+//a value in it
 //and type new one
 //it doesnt work
 //
-//might have to do with 
+//might have to do with
 //line 16
